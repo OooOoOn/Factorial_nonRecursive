1b7445470a91cc667f0d95515b0d9bbd0f6ee2cc
Jon <aadrianswe@gmail.com>
2017-09-25 21:08:25 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int number = scanner.nextInt();
+
+        System.out.println(factorial(number));
+    }
+
+    private static int factorial(int number) {
+        int result = number;
+        while(number > 1){
+            result *= (number - 1);
+            number--;
+        }
+        return result;
+    }
+
+
+}
